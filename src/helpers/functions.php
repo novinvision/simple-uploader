e175d4df5de22ab24618293f22b0ddab223858bb
Mehran Ranji <novinvision.com@gmail.com>
2025-11-23 16:22:57 +0330

    laravel 12 support

--- src/helpers/functions.php
+++ src/helpers/functions.php
@@ -3,6 +3,6 @@
 if(!function_exists('simple_upload_path')){
     function simple_upload_path($path): string
     {
-        return \App\Helpers\Storage::disk(config('simple-uploader.disk'))->path($path);
+        return \Illuminate\Support\Facades\Storage::disk(config('simple-uploader.disk'))->path($path);
     }
 }
